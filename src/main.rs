c0685843d06be207bf86fd5e4e55551c449786df
George Madrid <gmadrid@gmail.com>
2021-03-25 16:08:03 -0400

    Adding --output_filename cmd line option.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use argh::FromArgs;
 use itertools::Itertools;
 use std::collections::HashSet;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{self, BufRead, BufReader, Write};
 use std::iter::Extend;
 use std::path::PathBuf;
 use tot::{Record, Records, Result};
@@ -25,6 +25,10 @@ struct Args {
     #[argh(switch)]
     /// trim whitespace from beginning/end of values
     trim: bool,
+
+    #[argh(option, long = "output", short = 'o')]
+    /// output file. If not provided, output to stdout.
+    output_filename: Option<PathBuf>,
 
     #[argh(positional)]
     /// the file to process. (Currently only a single file is allowed.)
@@ -62,25 +66,40 @@ fn get_ordered_keys<'a>(first: &[&'a str], mut unordered: KeySet<'a>) -> Vec<&'a
     first.iter().chain(unordered.iter()).copied().collect()
 }
 
-fn spew_headers(keys: &[&str], separator: &str) {
-    println!("{}", keys.iter().join(separator));
+fn spew_headers(outstream: &mut impl Write, keys: &[&str], separator: &str) -> Result<()> {
+    writeln!(outstream, "{}", keys.iter().join(separator))?;
+    Ok(())
 }
 
-fn spew_records(keys: &[&str], records: &[Record], separator: &str) {
+fn spew_records(
+    outstream: &mut impl Write,
+    keys: &[&str],
+    records: &[Record],
+    separator: &str,
+) -> Result<()> {
     for rec in records {
-        println!(
+        writeln!(
+            outstream,
             "{}",
             keys.iter()
                 .map(|k| { rec.get(*k).map(|v| v.as_str()).unwrap_or("") })
                 .join(separator)
-        );
+        )?;
     }
+    Ok(())
 }
 
 fn get_instream(filename: &Option<PathBuf>) -> Result<Box<dyn BufRead>> {
     Ok(match filename {
         None => Box::new(BufReader::new(io::stdin())),
         Some(name) => Box::new(BufReader::new(File::open(name)?)),
+    })
+}
+
+fn get_outstream(filename: &Option<PathBuf>) -> Result<Box<dyn io::Write>> {
+    Ok(match filename {
+        None => Box::new(io::stdout()),
+        Some(name) => Box::new(File::create(name)?),
     })
 }
 
@@ -93,8 +112,10 @@ fn process() -> Result<()> {
     let first_keys = get_first_keys(&args.order);
     let ordered_keys = get_ordered_keys(&first_keys, all_key_set);
 
-    spew_headers(&ordered_keys, &args.output_separator);
-    spew_records(&ordered_keys, &recs, &args.output_separator);
+    let mut outstream = get_outstream(&args.output_filename)?;
+
+    spew_headers(&mut outstream, &ordered_keys, &args.output_separator)?;
+    spew_records(&mut outstream, &ordered_keys, &recs, &args.output_separator)?;
 
     Ok(())
 }
