edf529a7ccf95c62ed4071f57080231fc399913d
George Madrid <gmadrid@gmail.com>
2021-03-25 23:37:17 -0400

    More code simplification.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
+use std::io::{BufRead, BufReader, Read};
+
 mod chunker;
 mod kv;
-mod totter;
-
-//const SEPARATOR: char = ':';
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
@@ -15,4 +14,31 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub type Record = std::collections::HashMap<String, String>;
 pub type Records = Vec<Record>;
 
-pub use totter::read_records_from_chunks;
+pub fn read_records_from_chunks(
+    read: impl Read,
+    input_separator: &str,
+    trim: bool,
+) -> Result<Records> {
+    let lines = BufReader::new(read).lines().flatten();
+
+    let chunks = crate::chunker::chunks_at_blanks(lines);
+    let mut records: Vec<Record> = Default::default();
+    for chunk in chunks {
+        let record = record_from_chunk(chunk, input_separator, trim);
+        records.push(record);
+    }
+    Ok(records)
+}
+
+fn record_from_chunk<I>(chunk: I, input_separator: &str, trim: bool) -> Record
+where
+    I: Iterator<Item = String>,
+{
+    let mut record = Record::default();
+    for line in chunk {
+        let kv = kv::from_str(&line, input_separator);
+        let value = if trim { kv.value().trim() } else { kv.value() };
+        record.insert(kv.key().to_string(), value.to_string());
+    }
+    record
+}
